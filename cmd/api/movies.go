cdfaaba37804774675a560c75a3ef850bb4716a5
Hwiseok Seo <owl3670@gmail.com>
2022-02-10 23:00:18 +0900

    data 구조화 및 json response 형태 고정

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,18 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.alexedwards.net/internal/data"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request){
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new movie")
 }
 
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request){
-	id, err := app.readIDParam(r) 
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Cruella",
+		Runtime:   102,
+		Genres:    []string{"drama", "evil"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
